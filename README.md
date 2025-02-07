6872fb5410622a440ac38c88f6a9bda83e4e58e6
caofeibo- <690624138@qq.com>
2025-02-07 16:52:04 +0800

    处理android图标

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# ios-icon-maker
+# app-icon-maker
 
-通过一张指定的icon自动生成整套的ios图标(Xcode工程的Assets.xcassets/AppIcon.appiconset目录)
+通过一张指定的icon自动生成APP各分辨率的图标.
+- ios图标(Xcode工程的Assets.xcassets/AppIcon.appiconset目录),包含配置文件
+- android图标(src/main/res目录下)
 
-注意: **node版本需要大于18.17.0**
+注意: 
+- **node版本需要大于18.17.0**, 
+- 安卓图标没有考虑`xxx-xxx-v26`的文件夹,自适应图标会有点复杂
 
 
 ## 使用方法
 
-- 一般来说仅需要替换项目中的`icon.png`, 如有需要同时替换项目中的`Contents.json`
+- 一般来说仅需要替换项目中的`icon.png`, 如有需要同时替换项目中的`ios.json`或者`android.json`
 - 运行`npm i`
 - 运行`npm run build`
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const sharp = require('sharp');
-const contents = require('./Contents.json');
+const iosConfig = require('./ios.json');
+const androidConfig = require('./android.json')
 const sourceImage = './icon.png'
 const outputDir = './outputs';
 const tempArr = sourceImage.split('.');
@@ -23,31 +24,55 @@ function cleanDir(dir) {
   }
   console.log('make output directory...');
   fs.mkdirSync(dir);
+  fs.mkdirSync(`${dir}/ios`);
+  fs.mkdirSync(`${dir}/android`);
 }
 
 
 async function main() {
   cleanDir(outputDir);
-  const bar = new ProgressBar('export images [:bar] :current/:total :percent :etas', {
+  const iosBar = new ProgressBar('export ios icons [:bar] :current/:total :percent :elapseds', {
     complete: '=',
     incomplete: ' ',
     width: 20,
-    total: contents.images.length
+    total: iosConfig.images.length
   });
-  for (const element of contents.images) {
+  for (const element of iosConfig.images) {
     const [width, height] = element.size.split('x');
     const scale = element.scale.split('x')[0];
     const filename = `icon-${width}${scale > 1 ? '@' + element.scale : ''}.${fileExt}`;
     element.filename = filename;
     const result = await sharp(sourceImage)
       .resize(scale * width, scale * height)
-      .toFile(`${outputDir}/${filename}`);
+      .toFile(`${outputDir}/ios/${filename}`);
     if (result) {
-      bar.tick();
+      iosBar.tick();
     }
   }
-  
-  fs.writeFileSync(`${outputDir}/Contents.json`, JSON.stringify(contents, null, 2));
+  fs.writeFileSync(`${outputDir}/ios/Contents.json`, JSON.stringify(iosConfig, null, 2));
+  console.log('iOS icons exported successfully');
+
+  const androidBar = new ProgressBar('export ios icons [:bar] :current/:total :percent :elapseds', {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: androidConfig.images.length
+  });
+  for (const element of androidConfig.images) {
+    const [width, height] = element.size.split('x');
+    const filename = `${element.filename}.${fileExt}`;
+    const dir = `${outputDir}/android/${element.dir}`;
+    if (!isExistDir(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    const result = await sharp(sourceImage)
+      .resize(+width, +height)
+      .toFile(`${dir}/${filename}`);
+    if (result) {
+      androidBar.tick();
+    }
+  }
+  console.log('Android icons exported successfully');
 }
 
 main();
